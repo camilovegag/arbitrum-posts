9e4a20257d62f1f91ca0f50f8ae47292d5de43cd
camilovegag <camilovegag@unisabana.edu.co>
2023-12-04 00:52:54 -0500

    Add context for user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+import useAuth from "@/hooks/useAuth";
+
 export default function Home() {
+  const { authUser, isLogged } = useAuth();
   return (
     <section>
       <h1>Hola mundo</h1>
+      <pre>{JSON.stringify(authUser, null, 2)}</pre>
+      <pre>{JSON.stringify(isLogged)}</pre>
     </section>
   );
 }
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,7 +1,27 @@
+"use client";
+
 import styles from "./header.module.css";
+import useAuth from "@/hooks/useAuth";
+import { useAccount } from "wagmi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Header = () => {
+  const { setAuthUser, setIsLogged } = useAuth();
+
+  const account = useAccount({
+    onConnect({ address, isReconnected }) {
+      setAuthUser({
+        address,
+        likes: 0,
+      });
+      setIsLogged(true);
+    },
+    onDisconnect() {
+      setIsLogged(false);
+      setAuthUser(null);
+    },
+  });
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
